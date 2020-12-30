64ef76b6e13db2e7e27ca065bff9dbb232573d05
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-12-30 17:52:38 -0600

    improve: refactor to module auth

--- backend/src/auth/controllers/auth.controller.ts
+++ backend/src/auth/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { LocalAuthGuard } from './local-auth.guard';
-import { AuthService } from './auth.service';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 export class AuthController {
